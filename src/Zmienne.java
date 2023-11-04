17afe99248d9385e7b16fe99197da69a94b8347b
Dariusz Witkowski <witdar@wp.pl>
2023-11-04 19:01:54 +0100

    Jak w Java definiujemy podstawowe typy zmiennych

---
+++ src/Zmienne.java
@@ -0,0 +1,23 @@
+//zarezerwowane miejsce w pamieci z okreslonym typem danych
+public class Zmienne {
+
+    public static void main(String[] args) {
+
+        int firstNumber;
+        int secondNumber;
+        int result;
+
+        String name = "Bartek";
+        firstNumber = 1;
+        secondNumber = 2;
+        result = firstNumber + secondNumber;
+
+        name = "Darek";
+        System.out.println(name);
+        System.out.println(result);
+
+
+    }
+
+
+}
